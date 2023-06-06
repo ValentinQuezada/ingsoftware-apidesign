48f975aef1ea00a418c454f517caf3967b6ce945
sofgqv <sofia.garcia@utec.edu.pe>
2023-06-05 20:22:07 -0500

    check

--- temp_models.py
+++ temp_models.py
@@ -1,3 +1,6 @@
+users = []
+carts = {}
+
 class User:
     def __init__(self, username, password):
         self.username = username
@@ -54,13 +57,3 @@ products = [
     Product(3, "Pepsi", 2.0),
     Product(4, "7up", 1.5),
 ]
-
-users = [
-    User("paolov", "1234"),
-    User("paolov2", "1234")
-]
-
-carts = {
-    users[0] : ShoppingCart(users[0]),
-    users[1] : ShoppingCart(users[1])
-}
