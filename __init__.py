966ba3aba122570aefca5224e7eed0402f153c05
paolovasquezg <paolo.vasquez@utec.edu.pe>
2023-06-05 19:36:55 -0500

    inicio

---
+++ __init__.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify, abort
+import datetime
+from flask_cors import CORS
+import json
+import requests
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app, origin="*")
+
+    @app.route("/api/v1/user", methods=["GET"])
+
+    @app.route("/api/v1/products", methods=["GET"])
+
+    @app.route("/api/v1/shoppingcart", methods=["GET"])
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'statusCode': 404,
+            'message': 'resource not found'
+        }), 404
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+            'success': False,
+            'statusCode': 500,
+            'message': 'Internal Server error'
+        }), 500
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            'success': False,
+            'statusCode': 422,
+            'message': 'Unprocessable'
+        }), 422
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "statusCode": 400,
+            "message": "bad request"
+        }), 400
+
+
+    @app.route("/")
+    def main():
+        return "Api shopping"
+
+    return app
---
+++ run.sh
@@ -0,0 +1,4 @@
+#----------cambiar esto---------#
+cd /Users/paolovasquezgrahammer/Desktop/ingsoftware-apidesign
+export FLASK_APP=__init__.py
+flask run
